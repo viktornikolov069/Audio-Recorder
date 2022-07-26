8ccd87ee71508026a00b93afae3adc1ea7678600
viktornikolov069 <viktor.nikolov069@gmail.com>
2022-07-26 21:02:53 +0300

    Added full functionality to the AudioPlayerActivity

--- app/src/main/java/com/example/audiorecorder/ui/AudioPlayerActivity.kt
+++ app/src/main/java/com/example/audiorecorder/ui/AudioPlayerActivity.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.core.content.res.ResourcesCompat
 import androidx.room.Room
 import com.example.audiorecorder.R
@@ -14,6 +16,9 @@ import com.example.audiorecorder.db.AppDatabase
 import com.example.audiorecorder.db.AudioRecord
 import com.example.audiorecorder.utils.Constants
 import com.example.audiorecorder.utils.Constants.BUNDLE_AUDIO_RECORD_ID
+import com.google.android.material.appbar.MaterialToolbar
+import java.text.DecimalFormat
+import java.text.NumberFormat
 
 class AudioPlayerActivity : AppCompatActivity() {
 
@@ -34,7 +39,6 @@ class AudioPlayerActivity : AppCompatActivity() {
     /* Handler and Runnable */
     private lateinit var runnable: Runnable
     private lateinit var handler: Handler
-
 
     /* AudioRecord variables */
     private lateinit var record: AudioRecord
@@ -59,12 +63,26 @@ class AudioPlayerActivity : AppCompatActivity() {
         filename = appDB.audioRecordDao().getAll().find { it.id == id }!!.filename
         filePath = appDB.audioRecordDao().getAll().find { it.id == id }!!.filePath
 
+        /* Prepare toolbar */
+        binding.apply {
+            setSupportActionBar(mtToolbar)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            mtToolbar.setNavigationOnClickListener {
+                onBackPressed()
+            }
+
+            tvFilename.text = filename
+        }
+
         /* Preparing MediaPlayer */
         mediaPlayer = MediaPlayer()
         mediaPlayer.apply {
             setDataSource(filePath)
             prepare()
         }
+
+        binding.tvTrackDuration.text = dateFormat(mediaPlayer.duration)
 
         /* The handler delays a task that will be executed by the MainLooper */
         handler = Handler(Looper.getMainLooper())
@@ -74,6 +92,7 @@ class AudioPlayerActivity : AppCompatActivity() {
         runnable = Runnable {
             binding.apply {
                 seekBar.progress = mediaPlayer.currentPosition
+                tvTrackProgress.text = dateFormat(mediaPlayer.currentPosition)
                 handler.postDelayed(runnable, delay)
             }
         }
@@ -88,7 +107,8 @@ class AudioPlayerActivity : AppCompatActivity() {
         binding.seekBar.max = mediaPlayer.duration
 
         mediaPlayer.setOnCompletionListener {
-            binding.btnPlay.background = ResourcesCompat.getDrawable(resources, R.drawable.ic_pause_circle, theme)
+            binding.btnPlay.background = ResourcesCompat.getDrawable(resources,
+                R.drawable.ic_play_circle, theme)
         }
 
         binding.apply {
@@ -110,21 +130,60 @@ class AudioPlayerActivity : AppCompatActivity() {
                 mediaPlayer.playbackParams = PlaybackParams().setSpeed(playBackSpeed)
                 chipSpeed.text = "x $playBackSpeed"
             }
+
+            /* Change seekBar position */
+            seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    if (fromUser) mediaPlayer.seekTo(progress)
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                }
+            })
         }
     }
 
-    private fun playPausePlayer() {
+    /* --- Helper Functions --- */
 
+    private fun playPausePlayer() {
         // Not playing
         if (!mediaPlayer.isPlaying) {
              mediaPlayer.start()
             binding.btnPlay.background = ResourcesCompat.getDrawable(resources, R.drawable.ic_pause_circle, theme)
-            handler.postDelayed(runnable, 0)
+            handler.postDelayed(runnable, delay)
         } else {
             mediaPlayer.pause()
             binding.btnPlay.background = ResourcesCompat.getDrawable(resources, R.drawable.ic_play_circle, theme)
             handler.removeCallbacks(runnable)
         }
+    }
+
+    /* Goes back to the previous activity and stops the MediaPlayer */
+    override fun onBackPressed() {
+        super.onBackPressed()
+        mediaPlayer.stop()
+        mediaPlayer.release()
+        handler.removeCallbacks(runnable)
+    }
+
+    private fun dateFormat(duration: Int): String {
+        var durToSec = duration/1000 //ms to sec
+        var sec = durToSec % 60 // 6666sec % 60 = 6sec (extracting the seconds)
+        var min = (durToSec / 60) % 60 // 6666 / 60 = 111, 111 % 60 = 51 -> extracting the minutes
+        var hours = ((durToSec - min * 60) / 60 * 60 ).toInt()// extracting hours
+
+        val f: NumberFormat = DecimalFormat("00")
+        var str = "$min:${f.format(sec)}"
+
+        if (hours > 0) str = "$hours:$str"
+        return str
     }
 }
 
